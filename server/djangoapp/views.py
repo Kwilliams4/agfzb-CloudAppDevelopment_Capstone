94ea8becf2a78e63c1469fb83351844a35f8b3cf
Kwilliams4 <Kennepo23@gmail.com>
2023-11-24 09:35:06 -0500

    Changes to project

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,23 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
-# from .models import related models
-# from .restapis import related methods
+from .restapis import get_dealers_from_cf, get_dealer_reviews_from_cf, get_dealer_from_cf_by_id, post_request
 from django.contrib.auth import login, logout, authenticate
-from django.contrib import messages
-from datetime import datetime
 import logging
-import json
+from datetime import datetime
+from .models import CarModel
 
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
 
 # Create your views here.
-
-def home(request):
-    return render(request, 'djangoapp/index.html')
 
 
 # Create an `about` view to render a static about page
