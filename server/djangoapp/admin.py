4860818abbdf6d73f8b69477122ed1e5fb38cdd8
Kwilliams4 <Kennepo23@gmail.com>
2023-11-24 08:54:03 -0500

    Adding changes to project

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,20 +1,26 @@
 from django.contrib import admin
-#from .models import related models
+from .models import CarMake, CarModel
 
 
 # Register your models here.
-from .models import *
 
-# Register your models here.
 class CarModelInline(admin.StackedInline):
     model = CarModel
-    extra = 5
+    extra = 6
+
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['name',  'dealer_id', 'car_type', 'year']
+    search_fields = ['name']
 
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
+    list_display = ['name', 'description']
+    search_fields = ['name']
 
-admin.site.register(CarMake, CarMakeAdmin)
-admin.site.register(CarModel)
+# Register models here
+
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(CarMake)
 
 # CarModelInline class
 
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,33 +7,26 @@ app_name = 'djangoapp'
 urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
-    # name the URL
-
-    path('home', views.home, name='home'),
-    
+    # name the URL   
 
     # path for about view
 
-    path('about', views.about, name='about'),
+    path(route='about', view=views.about, name='about'),
 
-    # path for contact us view
+    path(route='contact', view=views.contact, name='contact'),
 
-    path('contact', views.contact, name='contact'),
+    path(route='registration', view=views.registration_request, name='registration'),
 
-    # path for registration
-    path('registration/', views.registration_request, name='registration'),
+    path(route='login', view=views.login_request, name='login'),
 
-    # path for login
-    path('login', views.login_request, name='login'),
-
-    # path for logout
-    path('logout', views.logout_request, name='logout'),
-
+    path(route='logout', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
-    # path for dealer reviews view
+    path('dealer/<int:dealer_id>/',
+         view=views.get_dealer_details, name='dealer_details'),
 
-    # path for add a review view
+    path('review/<int:dealer_id>/',
+         view=views.add_review, name='add_review'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
